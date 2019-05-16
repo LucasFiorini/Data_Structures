4d844e084241aab63e49281105ab6aa31ecd6857
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2019-05-16 19:41:44 +0000

    :hammer: :ok: Refactoring some things.

--- c++/List/include/list/list.hpp
+++ c++/List/include/list/list.hpp
@@ -1,28 +1,47 @@
+#ifndef LIST_H
+#define LIST_H 1
+
 #include "node.hpp"
-#ifndef LIST
-#define LIST
 
 typedef int Data;
 
 class List {
-	private:
-		Node* first;
-		Node* last;
-		unsigned int size;
+		
 	public:
+
 		List();
+
 		~List();
+		
 		void insert(Data d);
+		
 		void insertEnd(Data d);
+		
 		void insertHead(Data d);
+		
 		void insertIntoPos(int pos, Data d);
+		
 		void print();
-		inline bool empty();
+		
 		void remove(unsigned int pos);
-		bool search(Data d);
+		
 		void printReverse(Node* n, int node);
+		
 		void printReverse();
+		
+		bool search(Data d);
+		
 		bool operator==(const List& l);
+
+		inline bool empty();
+	
+	private:
+		
+		Node* first;
+		
+		Node* last;
+		
+		unsigned int size;
 };
 
 
